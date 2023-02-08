897c0c78352f2d23cbae79e4322396f294426fe7
Ilia Steblyan <stebljan@yandex.ru>
2023-02-08 21:47:06 +0600

    add models in core and realtions with user

--- drink_n_talk/core/models.py
+++ drink_n_talk/core/models.py
@@ -1,3 +1,130 @@
-# from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Language(models.Model):
+    """Языковая модель."""
+
+    name = models.CharField(
+        unique=True,
+        max_length=20,
+        verbose_name='Название'
+    )
+    abbreviation = models.SlugField(
+        max_length=5,
+        verbose_name='Аббревиатура'
+    )
+    users = models.ManyToManyField(
+        User,
+        through='UserLanguage',
+        verbose_name='Пользователь(и)'
+    )
+
+    class Meta:
+        """
+        Сортирует и добавляет названия в админке.
+        """
+        ordering = ('abbreviation',)
+        verbose_name = 'Язык'
+        verbose_name_plural = 'Языки'
+
+    def __repr__(self):
+        """
+        Добавляет удобочитаемый вывод при вызове экземпляра объекта
+        на печать и в оболочке shell.
+        """
+        return f'<{self.name} ({self.abbreviation})>'
+
+
+class UserLanguage(models.Model):
+    """
+    Промежуточная модель для присваивания пользователю разговорного языка.
+    """
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    language = models.ForeignKey(
+        Language,
+        on_delete=models.CASCADE,
+        verbose_name='Язык'
+    )
+
+    class Meta:
+        """
+        Добавляет названия в админке.
+        """
+        verbose_name = 'Пользователь + язык'
+        verbose_name_plural = 'Пользователи + языки'
+    
+    def __repr__(self):
+        """
+        Добавляет удобочитаемый вывод при вызове экземпляра объекта
+        на печать и в оболочке shell.
+        """
+        return f'<{self.user} ({self.language})>'
+
+
+class Drink(models.Model):
+    """Модель для напитков."""
+
+    title = models.CharField(
+        max_length=20,
+        verbose_name='Название'
+    )
+    degree = models.IntegerField(
+        choices=settings.DEGREE_CHOICES,
+        verbose_name='Степень алкогольности'
+    )
+
+    class Meta:
+        """
+        Сортирует и добавляет названия в админке.
+        """
+        ordering = ('title',)
+        verbose_name = 'Напиток'
+        verbose_name_plural = 'Напитки'
+
+    def __repr__(self):
+        """
+        Добавляет удобочитаемый вывод при вызове экземпляра объекта
+        на печать и в оболочке shell.
+        """
+        return f'<{self.title} ({self.degree})>'
+
+
+class UserDrink(models.Model):
+    """
+    Промежуточная модель для присваивания пользователю
+    предпочитаемых напитков.
+    """
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    drink = models.ForeignKey(
+        Drink,
+        on_delete=models.CASCADE,
+        verbose_name='Напиток'
+    )
+
+    class Meta:
+        """
+        Добавляет названия в админке.
+        """
+        verbose_name = 'Пользователь + напиток'
+        verbose_name_plural = 'Пользователи + напитки'
+    
+    def __repr__(self):
+        """
+        Добавляет удобочитаемый вывод при вызове экземпляра объекта
+        на печать и в оболочке shell.
+        """
+        return f'<{self.user} + {self.drink}>'
--- drink_n_talk/users/models.py
+++ drink_n_talk/users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,21 +6,32 @@ from django.db import models
 class User(AbstractUser):
     """Кастомизация базовой модели пользователя."""
 
-    birth_day = models.DateField()
+    birth_day = models.DateField(
+        verbose_name='День рождения'
+    )
     about = models.TextField(
-        blank=True
+        blank=True,
+        verbose_name='О себе'
     )
     photo = models.ImageField(
-        upload_to='users_photos',
-        default='default_user.jpg'
+        upload_to='/users_photos',
+        default='default_user.jpg'б
+        verbose_name='Аватар'
     )
-
-    def __repr__(self):
-        """
-        Добавляет удобочитаемый вывод при вызове экземпляра объекта
-        на печать и в оболочке shell.
-        """
-        return f'< userid = {self.id}  | username = {self.username} >'
+    languages = models.ManyToManyField(
+        'Language',
+        through='UserLanguage',
+        verbose_name='Язык(и)'
+    )
+    drinks = models.ManyToManyField(
+        'Drink',
+        through='UserDrink',
+        verbose_name='Напиток(и)'
+    )
+    degree = models.IntegerField(
+        choices=settings.DEGREE_CHOICES,
+        verbose_name='Степень алкогольности'
+    )
 
     class Meta:
         """
@@ -28,3 +40,10 @@ class User(AbstractUser):
         ordering = ('-id',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def __repr__(self):
+        """
+        Добавляет удобочитаемый вывод при вызове экземпляра объекта
+        на печать и в оболочке shell.
+        """
+        return f'<user.id = {self.id}  | username = {self.username}>'
