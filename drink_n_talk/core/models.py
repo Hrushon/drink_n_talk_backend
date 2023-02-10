53215b90daa535fce98091b6fcb1b4a26d272e20
Ilia Steblyan <stebljan@yandex.ru>
2023-02-10 23:25:44 +0600

    fix(core): linter errors

--- drink_n_talk/core/models.py
+++ drink_n_talk/core/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 User = get_user_model()
@@ -94,7 +94,7 @@ class Drink(models.Model):
         User,
         through='UserDrink',
         verbose_name='пользователь(и)'
-    )    
+    )
 
     class Meta:
         """
