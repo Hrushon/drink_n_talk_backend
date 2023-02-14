1d785b9a1f2440c7b0c1aa56fee8d4bd59dbdf9d
Ilia Steblyan <stebljan@yandex.ru>
2023-02-14 23:29:44 +0600

    fear(infra)!: added dynamic redoc and infra
    Create Dockerfile & docker-compose.yaml for deploing.
    Create first start on containers: postgres, nginx & backend.
    
    Added `gunicorn`.

--- drink_n_talk/drink_n_talk/urls.py
+++ drink_n_talk/drink_n_talk/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 urlpatterns = [
@@ -11,19 +11,19 @@ urlpatterns = [
 ]
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Drink&Talk API",
-      default_version='v1',
-      description="Документация для проекта D&T",
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email="i@steblyan.ru"),
-      license=openapi.License(name="MIT License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Drink&Talk API",
+        default_version='v1',
+        description="Документация для проекта D&T",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="i@steblyan.ru"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns += [
-   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
-       name='schema-redoc'),
-] 
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
+]
